7f97f4232e806de59f284db37642db9a5c677975
Mr.Nobody <mrcartoonster@yahoo.com>
2021-06-24 02:40:49 +0000

    📦 NEW: DR-1 #time 1h #comment Function(api): Adding defaults to response

--- api/business/business_days.py
+++ api/business/business_days.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+from typing import Optional
+
+import pendulum as p
 from fastapi import APIRouter
 from fastapi.responses import ORJSONResponse
 from pydantic import BaseModel, Field
 
-# from services.business.add_working import working_days
+from services.business.add_working import working_days
 
 router = APIRouter(
     prefix="/business",
@@ -12,6 +15,7 @@ router = APIRouter(
 )
 
 # Clean this up!!!
+# Regex for YYYY-MM-DDDD or MM-DDDD-YYYY
 proper_dates = (
     r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])|"
     r"^(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])-\d{4}"
@@ -19,8 +23,8 @@ proper_dates = (
 
 
 class WorkingDays(BaseModel):
-    date: str = Field(
-        ...,
+    date: Optional[str] = Field(
+        p.now().to_date_string(),
         regex=proper_dates,
         description="Date to count from for business days.",
     )
@@ -31,8 +35,9 @@ class WorkingDays(BaseModel):
 
 
 @router.post("/")
-async def business_day(date: str = WorkingDays):
+async def business_day(date: WorkingDays):
     """
     Calculate working days from given date with given number of days.
     """
-    ...
+    days = working_days(first_date=date.date, num=date.number)
+    return {"date": days}
